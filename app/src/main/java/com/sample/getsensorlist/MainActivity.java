500749c49188318e7653009aed2cf5d3c53bb392
HyeJin <rkd2313@gmail.com>
2021-04-06 12:27:26 +0900

    get sensor list using onCreate > onClick

--- app/src/main/java/com/sample/getsensorlist/MainActivity.java
+++ app/src/main/java/com/sample/getsensorlist/MainActivity.java
@@ -7,12 +7,13 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView mTextSensors;
     SensorManager sensorMgr;
     List<Sensor> sensorList;
@@ -27,19 +28,18 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.retrieve_sensors_btn);
 //        mTextSensors.setMovementMethod(new ScrollingMovementMethod());
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
-                sensorList = sensorMgr.getSensorList(Sensor.TYPE_ALL);
+        button.setOnClickListener(this);
+    }
 
-                mTextSensors.append("(# Sensors: " + sensorList.size() + ")\n\n");
-                for (Sensor sensor : sensorList) {
-                    mTextSensors.append("Sensor name: " + sensor.getName() + "\n");
-                    mTextSensors.append("Sensor type: " + sensor.getType() + "\n\n");
+    @Override
+    public void onClick(View v) {
+        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorList = sensorMgr.getSensorList(Sensor.TYPE_ALL);
 
-                }
-            }
-        });
+        mTextSensors.append("(# Sensors: " + sensorList.size() + ")\n\n");
+        for (Sensor sensor : sensorList) {
+            mTextSensors.append("Sensor name: " + sensor.getName() + "\n");
+            mTextSensors.append("Sensor type: " + sensor.getType() + "\n\n");
         }
     }
+}
